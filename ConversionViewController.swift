b5b90cd0e1dca44344c1c56327b3343de7e15d53
Henngel Mojica <henngelm@icloud.com>
2018-02-14 17:14:40 -0300

    Text input. Type in UITextField and update celsiusLabel

---
+++ ConversionViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ConversionViewController.swift
+//  WorldTrotterDemo
+//
+//  Created by Henngel Mojica on 2/14/18.
+//  Copyright © 2018 Henngel Mojica. All rights reserved.
+//
+
+import UIKit
+
+class ConversionViewController: UIViewController {
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet var textField: UITextField!
+    
+    @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField){
+        if let text = textField.text, !text.isEmpty {
+            celsiusLabel.text = text
+        } else {
+            celsiusLabel.text = "???"
+        }
+        
+    }
+    
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+}
